45bafc8454b78a8e1690eaf2a95753170f07de9f
santoshchintu265-create <santoshchintu265@gmail.com>
2026-08-19 12:12:58 +0530

    Create students management UI

--- app.py
+++ app.py
@@ -1,11 +1,26 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def home():
-    return "QR Attendance System is running!"
+    return render_template("index.html")
+
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+
+@app.route("/dashboard")
+def dashboard():
+    return render_template("dashboard.html")
+
+
+@app.route("/students")
+def students():
+    return render_template("students.html")
 
 
 if __name__ == "__main__":
